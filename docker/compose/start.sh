ab467d4791eaea2a3142d62231a8b88503525dda
Grzegorz Szadkowski <5392918+gszadkow@users.noreply.github.com>
2018-11-06 15:09:37 +0100

    Improving start script

--- docker/compose/start.sh
+++ docker/compose/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+INIT_INFLUX_DB=TRUE
+INIT_HORIZON=TRUE
+INIT_STELLAR_DB=TRUE
+
 export STELLAR_ROOT=${1:-$STELLAR_HOME}
 export TARGET=${2:-test}
 export WORKSPACE=$STELLAR_ROOT/$TARGET
@@ -10,48 +14,48 @@ export USER_ME=$(id -u)
 export GROUP_ME=$(id -g)
 export DOMAIN=${TARGET}.intranet.stellar-fox.net
 
-mkdir -p $DATA_ROOT/chronograf
-chmod a+w $DATA_ROOT/chronograf
-mkdir -p $DATA_ROOT/deneb-db
-chmod a+w $DATA_ROOT/deneb-db
-mkdir -p $DATA_ROOT/grafana
-chmod a+w $DATA_ROOT/grafana
-mkdir -p $DATA_ROOT/horizon-db
-chmod a+w $DATA_ROOT/horizon-db
-mkdir -p $DATA_ROOT/influxdb
-chmod a+w $DATA_ROOT/influxdb
-mkdir -p $DATA_ROOT/kapacitor
-chmod a+w $DATA_ROOT/kapacitor
-mkdir -p $DATA_ROOT/nginx
-chmod a+w $DATA_ROOT/nginx
-mkdir -p $DATA_ROOT/pgadmin
-chmod a+w $DATA_ROOT/pgadmin
-mkdir -p ${DATA_ROOT}/stellar-core-db
-chmod a+w ${DATA_ROOT}/stellar-core-db
-mkdir -p ${DATA_ROOT}/stellar-core-horizon
-chmod a+w ${DATA_ROOT}/stellar-core-horizon
+ROOTS=(${LOG_ROOT} ${DATA_ROOT})
+CONTAINER_NAMES=$(cat docker-compose.yml | grep "container_name:" | awk '{print $2}' | sed -e 's/fox_//')
 
-mkdir -p $LOG_ROOT/fluentd
-chmod a+w $LOG_ROOT/fluentd
-mkdir -p $LOG_ROOT/nginx
-chmod a+w $LOG_ROOT/nginx
-mkdir -p $LOG_ROOT/pgadmin
-chmod a+w $LOG_ROOT/pgadmin
-
-# Influx as backend of the logging must be started first
+echo "Creating following writable directories:"
+for ROOT in ${ROOTS[*]}; do
+  for CONTAINER_NAME in ${CONTAINER_NAMES}; do
+    DIRECTORY=${ROOT}/${CONTAINER_NAME}
+    if [ ! -d "${DIRECTORY}" ]; then
+      echo "- ${DIRECTORY}"
+      mkdir -p ${DIRECTORY}
+      chmod a+w ${DIRECTORY}
+    fi
+  done
+done
+echo "done."
+echo "Influx as backend of the logging must be started first"
 docker-compose up -d influxdb
+sleep 2
 
-# The DBs backing core and horizon must be started before initialization
+if [ $INIT_INFLUX_DB == "TRUE" ]; then
+  echo "Creating InfluxDB"
+  curl -POST 'http://localhost:8086/query' --data-urlencode "q=CREATE DATABASE logs_collector"
+fi
+
+echo "Fluentd must connect to influx before the other services start to log to it"
+docker-compose up -d fluentd
+sleep 2
+
+echo "The DBs backing core and horizon must be started before initialization"
 docker-compose up -d horizon-db
 docker-compose up -d stellar-core-db
 sleep 5
-curl -POST 'http://localhost:8086/query' --data-urlencode "q=CREATE DATABASE logs_collector"
-docker run --network="compose_default" -v ${WORKSPACE}/etc/stellar-core.cfg:/etc/stellar-core.cfg -it stellar-core:latest /bin/bash -c "stellar-core --conf /etc/stellar-core.cfg --newdb"
-docker run --network="compose_default" -it horizon:latest /bin/bash -c "horizon db init --db-url=\"dbname=horizon user=horizon password=horizon host=fox_horizon_db port=5432 sslmode=disable\""
 
-# Fluentd must connect to influx before the other services start to log to it
-docker-compose up -d fluentd
-sleep 2
+if [ $INIT_STELLAR_DB == "TRUE" ]; then
+  echo "Initializing stellar-core DB"
+  docker run --network="compose_default" -v ${WORKSPACE}/etc/stellar-core.cfg:/etc/stellar-core.cfg -it stellar-core:latest /bin/bash -c "stellar-core --conf /etc/stellar-core.cfg --newdb"
+fi
+
+if [ $INIT_HORIZON == "TRUE" ]; then
+  echo "Initializing horizon DB"
+  docker run --network="compose_default" -it horizon:latest /bin/bash -c "horizon db init --db-url=\"dbname=horizon user=horizon password=horizon host=fox_horizon_db port=5432 sslmode=disable\""
+fi
 
 # The rest is starting
 docker-compose up -d

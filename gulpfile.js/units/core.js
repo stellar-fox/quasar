c0ea78af6851bf49e39cd43e4e20e2e9517dc2e7
Grzegorz Szadkowski <5392918+gszadkow@users.noreply.github.com>
2018-11-16 14:46:52 +0100

    Coding style adjustment: core

--- gulpfile.js/units/core.js
+++ gulpfile.js/units/core.js
@@ -1,53 +1,99 @@
-const gulp = require('gulp')
-const config = require('../lib/utils').cfg()
-const child_process = require("child_process");
-const mkdirp = require('mkdirp');
-const yaml = require('js-yaml');
-const { series } = require('gulp');
+/**
+ * Quasar.
+ *
+ * Gulp unit for stellar-core.
+ *
+ * @module gulp-stellar-core-lib
+ * @license Apache-2.0
+ */
 
-const docker_compose_cmd = "docker-compose -f " + config['QUASAR_ROOT'] 
-    + "/docker/compose/docker-compose.yml "
 
-function core_db_up(cb) {
-    const cmd = docker_compose_cmd + "up -d stellar-core-db"
-    child_process.execSync(cmd, {'env': config}).toString()
+
+
+const 
+    gulp = require("gulp"),
+    config = require("../lib/utils").cfg(),
+    child_process = require("child_process"),
+    mkdirp = require("mkdirp"),
+    yaml = require("js-yaml"),
+    { series } = require("gulp"),
+    { string } = require("@xcmats/js-toolbox")
+
+
+
+
+
+// ...
+let docker_compose_cmd_prefix =
+    "docker-compose -f " +
+    `${config.QUASAR_ROOT}/docker/compose/docker-compose.yml`
+
+
+
+
+// ...
+function core_db_up (cb) {
+    const cmd = `${docker_compose_cmd_prefix} up -d stellar-core-db`
+    child_process.execSync(cmd, {"env": config}).toString()
     // Lets give it some time to spawn it
     setTimeout(cb, 4000)
 }
 
-function core_db_rm(cb) {
-    const cmd = docker_compose_cmd + "rm stellar-core-db"
-    const a = child_process.execSync(cmd, {'env': config}).toString()
+
+
+
+// ...
+function core_db_rm (cb) {
+    const cmd = `${docker_compose_cmd_prefix} rm stellar-core-db`
+    const a = child_process.execSync(cmd, {"env": config}).toString()
     console.log(a)
     cb()
 }
 
-function core_db_init(cb) {
-    const cmd = docker_compose_cmd
-        + "run --rm  stellar-core /bin/bash -c \"stellar-core --conf /etc/stellar-core.cfg --newdb\""
-    const a = child_process.execSync(cmd, {
-            'env': Object.assign({}, { 'PATH': process.env.PATH }, config)
+
+
+
+// ...
+function core_db_init (cb) {
+    const 
+        cmd = `${docker_compose_cmd_prefix} run --rm  stellar-core /bin/bash -c `
+            + string.quote("stellar-core --conf /etc/stellar-core.cfg --newdb"),
+        a = child_process.execSync(cmd, {
+            env: { PATH: process.env.PATH, ...config },
         }).toString()
     console.log(a)
     cb()
 }
 
-function core_config_show(cb) {
-    const cmd = docker_compose_cmd + "config"
-    const a = child_process.execSync(cmd, {'env': config}).toString()
-    const b = yaml.safeDump({ 
-        'stellar-core-db': yaml.safeLoad(a)['services']['stellar-core-db'],
-        'stellar-core': yaml.safeLoad(a)['services']['stellar-core'] 
-    })
+
+
+
+// ...
+function core_config_show (cb) {
+    const 
+        cmd = `${docker_compose_cmd_prefix} config`,
+        a = child_process.execSync(cmd, {"env": config}).toString(),
+        b = yaml.safeDump({ 
+            "stellar-core-db": yaml.safeLoad(a)["services"]["stellar-core-db"],
+            "stellar-core": yaml.safeLoad(a)["services"]["stellar-core"],
+        })
     console.log(b)
     cb()
 }
 
-function core_dir(cb) {
-    mkdirp(config['DATA_ROOT'] + '/stellar-core/history')
-    mkdirp(config['DATA_ROOT'] + '/stellar-core-db')
-    cb();
+
+
+
+// ...
+function core_dir (cb) {
+    mkdirp(`${config["DATA_ROOT"]}/stellar-core/history`)
+    mkdirp(`${config["DATA_ROOT"]}/stellar-core-db`)
+    cb()
 }
 
-gulp.task('core_config_show', core_config_show)
-gulp.task('core_init', series(core_config_show, core_dir, core_db_up, core_db_init, core_db_rm))
+
+
+
+// ...
+gulp.task("core_config_show", core_config_show)
+gulp.task("core_init", series(core_config_show, core_dir, core_db_up, core_db_init, core_db_rm))

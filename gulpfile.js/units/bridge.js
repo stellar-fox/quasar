1ccaa0974c981ee0393d4adb6d8aa9479eecb2f5
drmats <drmats@users.noreply.github.com>
2018-11-16 13:18:56 +0100

    units/bridge: initially linted.

--- gulpfile.js/units/bridge.js
+++ gulpfile.js/units/bridge.js
@@ -1,54 +1,112 @@
-const gulp = require('gulp')
-const config = require('../lib/utils').cfg()
-const child_process = require("child_process");
-const mkdirp = require('mkdirp');
-const yaml = require('js-yaml');
-const { series } = require('gulp');
+/**
+ * Quasar.
+ *
+ * Gulp unit for bridge.
+ *
+ * @module gulp-bridge-lib
+ * @license Apache-2.0
+ */
 
-const docker_compose_cmd = "docker-compose -f " + config['QUASAR_ROOT'] 
-    + "/docker/compose/docker-compose.yml "
 
-function bridge_db_up(cb) {
-    const cmd = docker_compose_cmd + "up -d bridge-db"
-    child_process.execSync(cmd, {'env': config}).toString()
+
+
+const
+    gulp = require("gulp"),
+    config = require("../lib/utils").cfg(),
+    child_process = require("child_process"),
+    mkdirp = require("mkdirp"),
+    yaml = require("js-yaml"),
+    { series } = require("gulp"),
+    { string } = require("@xcmats/js-toolbox")
+
+
+
+
+// ...
+let docker_compose_cmd_prefix =
+    "docker-compose -f " +
+    `${config.QUASAR_ROOT}/docker/compose/docker-compose.yml`
+
+
+
+
+// ...
+function bridge_db_up (cb) {
+    const cmd = `${docker_compose_cmd_prefix} up -d bridge-db`
+
+    child_process.execSync(cmd, { env: config }).toString()
     // Lets give it some time to spawn it
     setTimeout(cb, 4000)
 }
 
-function bridge_db_rm(cb) {
-    const cmd = docker_compose_cmd + "rm bridge-db"
-    const a = child_process.execSync(cmd, {'env': config}).toString()
+
+
+
+// ...
+function bridge_db_rm (cb) {
+    const
+        cmd = `${docker_compose_cmd_prefix} rm bridge-db`,
+        a = child_process.execSync(cmd, { env: config }).toString()
+
     console.log(a)
     cb()
 }
 
-function bridge_db_init(cb) {
-    const cmd = docker_compose_cmd
-        + "run --rm  bridge /bin/bash -c "
-        + "\"bridge --config /etc/bridge.cfg --migrate-db\""
-    const a = child_process.execSync(cmd, {
-            'env': Object.assign({}, { 'PATH': process.env.PATH }, config)
+
+
+
+// ...
+function bridge_db_init (cb) {
+    const
+        cmd = `${docker_compose_cmd_prefix} run --rm  bridge /bin/bash -c ` +
+            string.quote("bridge --config /etc/bridge.cfg --migrate-db"),
+        a = child_process.execSync(cmd, {
+            env: { PATH: process.env.PATH, ...config },
         }).toString()
+
     console.log(a)
     cb()
 }
 
-function bridge_config_show(cb) {
-    const cmd = docker_compose_cmd + "config"
-    const a = child_process.execSync(cmd, {'env': config}).toString()
-    const b = yaml.safeDump({ 
-        'bridge-db': yaml.safeLoad(a)['services']['bridge-db'],
-        'bridge': yaml.safeLoad(a)['services']['bridge'] 
-    })
+
+
+
+// ...
+function bridge_config_show (cb) {
+    const
+        cmd = `${docker_compose_cmd_prefix} config`,
+        a = child_process.execSync(cmd, { "env": config }).toString(),
+        b = yaml.safeDump({
+            "bridge-db": yaml.safeLoad(a)["services"]["bridge-db"],
+            "bridge": yaml.safeLoad(a)["services"]["bridge"],
+        })
+
     console.log(b)
     cb()
 }
 
-function bridge_dir(cb) {
-    mkdirp(config['DATA_ROOT'] + '/bridge/history')
-    mkdirp(config['DATA_ROOT'] + '/bridge-db')
-    cb();
+
+
+
+// ...
+function bridge_dir (cb) {
+    mkdirp(config.DATA_ROOT + "/bridge/history")
+    mkdirp(config.DATA_ROOT + "/bridge-db")
+    cb()
 }
 
-gulp.task('bridge_config_show', bridge_config_show)
-gulp.task('bridge_init', series(bridge_config_show, bridge_dir, bridge_db_up, bridge_db_init, bridge_db_rm))
+
+
+
+// ...
+gulp.task("bridge_config_show", bridge_config_show)
+gulp.task(
+    "bridge_init",
+    series(
+        bridge_config_show,
+        bridge_dir,
+        bridge_db_up,
+        bridge_db_init,
+        bridge_db_rm
+    )
+)

404ae110ab02482426fc6e4f0461c6d4dccff70f
Grzegorz Szadkowski <5392918+gszadkow@users.noreply.github.com>
2018-11-22 19:49:52 +0100

    Build fusion with shambala workaround

---
+++ docker/images/builder/build_fusion.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "Usage:"
+echo "./run "
+docker rm -f builder
+WORKSPACE=`readlink -f ../../../..`
+OUTPUT=fusion_build_`date +"%Y%m%d%H%M%S"`
+echo $WORKSPACE
+echo $OUTPUT
+docker run \
+    --name builder \
+    -e "HOME=/workspace" -u $UID \
+    -v $WORKSPACE:/workspace \
+    -w /workspace \
+    builder:latest /bin/bash -c \
+"""
+cd /workspace/shambhala
+npm run clean
+npm i
+npm run build:lib
+cd -
+rm /workspace/fusion/src/lib/shambhala.client.js
+ln -s /workspace/shambhala/dist.lib/shambhala.client.js /workspace/fusion/src/lib/shambhala.client.js
+/files/buildscript.sh /workspace fusion fusion_build_`date +"%Y%m%d%H%M%S"`
+"""
+
